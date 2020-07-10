ed9f7fa7c31db3387eecf1160e582eb728593d09
Jender <jenderzavala@gmail.com>
2020-07-10 19:12:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Features
+
+### Coding & Deployment
+
+- Using firebase `firestore` database with realtime update
+- Single page progressive ecommerce app using VueJs
+- Offline enabled
+- Progressive images with lazy loading
+- All assets(js,css,images) are precached for a faster user experience
+- Modular coding structure
+- Auto deploy to Netlify production server
+- Better and cleaner JavaScript code
+- Simplified installation and configuration
+- Reliable: Load fast and provide offline experience
+- Fast: Respond quickly to user actions
+- Engaging: Feel like a native app on mobile devices
+- Integrated google analytics
+
+### Store Front
+
+- Checkout using COD
+- Customer Address
+- Responsive store front
+- Dynamic Page Titles
+- Firebase authentication system (Login with google)
---
+++ config.js
@@ -0,0 +1,73 @@
+const pkg = require('./package')
+
+module.exports = {
+  apiKey: 'AIzaSyCgwAhTr_xxihZ47AG5Zv4Z5EGkeKPqPJk',
+  projectId: 'delivery-e7702',
+  authDomain: 'delivery-e7702.firebaseapp.com',
+  ANALYTICS_TRACKING_ID: '',
+  clearCart: true, // Whether to clear the cart after order is placed. Useful while testing
+  currency: { symbol: '$', code: 'USD' },
+  manifest: {
+    name: 'Enksa',
+    short_name: 'Enksa',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#555'
+  },
+  head: {
+    titleTemplate: 'Enksa | %s',
+    title: pkg.description,
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Karla:400,700'
+      }
+    ],
+    meta: [
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'keyword', name: 'keyword', content: pkg.keyword },
+      { hid: 'description', name: 'description', content: pkg.description },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: pkg.description
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: pkg.description
+      },
+      // {
+      //   property: "og:image",
+      //   content:
+      //     "https://s3.envato.com/files/252462089/banner.png"
+      // },
+      {
+        hid: 'twitter:title',
+        property: 'twitter:title',
+        content: pkg.description
+      },
+      {
+        hid: 'twitter:description',
+        property: 'twitter:description',
+        content: pkg.description
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://foodfire.info'
+      }
+    ],
+    noscript: [
+      {
+        innerHTML: `We're sorry but Foodfire doesn't work properly without JavaScript enabled. Please enable it to continue.`
+      }
+    ],
+    htmlAttrs: { lang: 'es' },
+    __dangerouslyDisableSanitizers: ['script']
+  }
+}
