250565d3086cf16b2c0ec7ca190aeb26367485b1
Jender <jenderzavala@gmail.com>
2020-07-26 00:19:40 -0400

    UI color

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar class="is-light">
+  <b-navbar class="is-danger">
     <template slot="brand">
      <b-navbar-item tag="router-link" :to="{ path: '/' }">
                 <img
